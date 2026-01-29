5be5e07178b01be0c159037329692ec5961e13fc
FTT95 <41843637+FadyT@users.noreply.github.com>
2026-01-29 13:22:58 +0200

    Testing avaturn

---
+++ docs/TemplateData/UnitySetup.js
@@ -0,0 +1,25 @@
+function unityShowBanner(msg, type) {
+        
+    }
+    
+    var buildUrl = "Build";
+    var loaderUrl = buildUrl + "/webglbuild.loader.js";
+    var config = {
+        dataUrl: buildUrl + "/webglbuild.data.br",
+        frameworkUrl: buildUrl + "/webglbuild.framework.js.br",
+        codeUrl: buildUrl + "/webglbuild.wasm.br",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "My project",
+        productVersion: "0.1.0",
+        showBanner: unityShowBanner,
+    };
+
+    createUnityInstance(canvas, config, (progress) => {
+    })
+        .then((unityInstance) => {
+            gameInstance = unityInstance;
+        })
+        .catch((message) => {
+            alert(message);
+        });
